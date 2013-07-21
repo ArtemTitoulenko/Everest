fc8798e7e735726bf6cd7535e0ab92c7bd05ab69
ArtemTitoulenko <artem.titoulenko@gmail.com>
2013-07-21 02:19:48 -0700

    db.js for everything

--- server.js
+++ server.js
@@ -8,35 +8,23 @@ app.use(express.logger())
 
 // middleware which passes a reference to the db in the request object
 // there's probably a better way to do this
-var db = require('monk')(config.mongo.host + '/' + config.mongo.db)
+var db = require('./lib/db')(app, config)
 app.use(function (req, res, next) {
   req.db = db
   next()
 })
 
-// middleware that injects a reference to the redis client
-var redis = require('redis')
-var redisClient = redis.createClient()
-app.use(function (req, res, next) {
-  req._redis = redis // for raw redis access
-  req.redis = redisClient
-  next()
-})
-
-// set views for error and 404 pages
-app.set('views', __dirname + '/views');
-
 // session support
-app.use(express.cookieParser(config.server.cookie_secret));
-app.use(express.session());
+app.use(express.cookieParser(config.server.cookie_secret))
+app.use(express.session())
 
 // parse request bodies (req.body)
-app.use(express.bodyParser());
+app.use(express.bodyParser())
 
 // support _method (PUT in forms etc)
-app.use(express.methodOverride());
+app.use(express.methodOverride())
 
-require('./boot')(app, {})
+require('./lib/boot')(app, {})
 
 // serve static files
 app.use(express.static(__dirname + '/public'))
@@ -44,6 +32,9 @@ app.use(express.static(__dirname + '/public'))
 app.use('*', function (req, res, next) {
   res.send(404, '404')
 })
+
+// set views for error and 404 pages
+app.set('views', __dirname + '/views')
 
 if (!module.parent) {
   app.listen(config.server.port)
