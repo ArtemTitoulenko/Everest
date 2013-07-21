5e8c62b1a141413451d20c87d076a35136b1399c
ArtemTitoulenko <artem.titoulenko@gmail.com>
2013-07-21 14:47:23 -0700

    I think this is a decent way to do models

--- app/models/post.js
+++ app/models/post.js
@@ -1,3 +1,29 @@
 // fuck this shit
+// mongodb things are schema-less. so why bother writing a schema??! :wq yolo
+// validation, is what we really want
 
-// mongodb things are schema-less. so why bother writing a schema??! :wq
+var schema = require('js-schema')
+
+function Post(data) {
+  var valid = schema({
+    title: String,
+    authorId: Number,
+    body: String // should be Array.of(Paragraph), but not yet   
+  })(data)
+
+  if (valid) for (var key in data) this[key] = data[key]
+  else throw new Error('Not a valid Post')
+}
+
+Post.fromJson = function(data) {
+  return new Post(data)
+}
+
+module.exports = Post
+
+/* testing */
+var p = new Post({title: 'foo', authorId: 123, body: 'bar'})
+console.log(p)
+
+p = Post.fromJson({title: 'foo', authorId: 123, body: 'bar'})
+console.log(p)
