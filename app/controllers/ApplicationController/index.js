dc43df2bb74019eb3c334e538d0f81ff4272cee1
ArtemTitoulenko <artem.titoulenko@gmail.com>
2013-07-21 16:03:43 -0700

    better controllers? still exploring

--- app/controllers/ApplicationController/index.js
+++ app/controllers/ApplicationController/index.js
@@ -1,27 +1,32 @@
-var db = require('../../../lib/db.js').getInstance()
+function ApplicationController(db) {
+  this.db = db
 
-module.exports = {
-  routes: [
+  this.routes = [
     {method: 'get', path: '/', action: 'index'},
     {method: 'get', path: '/hello', action: 'hello'},
     {method: 'get', path: '/hello/:name', action: 'helloName'}
-  ],
+  ]
 
-  index: function (req, res) {
-    res.send('home page')
-  },
-
-  hello: function(req, res) {
-    db.redis.incr('hello', function (err, obj) {
-      console.log('hello:', obj)
-    })
-    res.send('hello world')
-  },
-
-  helloName: function(req, res) {
-    db.redis.incr('helloName', function (err, obj) {
-      console.log('helloName:', obj)
-    })
-    res.send('hello ' + req.params.name)
-  }
+  return this
 }
+
+ApplicationController.prototype.index = function (req, res) {
+  res.send('home page')
+}
+
+ApplicationController.prototype.hello = function (req, res) {
+  console.log(this.db.mongo)
+  this.db.redis.incr('hello', function (err, obj) {
+    console.log('hello:', obj)
+  })
+  res.send('hello world')
+}
+
+ApplicationController.prototype.helloName = function (req, res) {
+  this.db.redis.incr('helloName', function (err, obj) {
+    console.log('helloName:', obj)
+  })
+  res.send('hello ' + req.params.name)
+}
+
+module.exports = ApplicationController
